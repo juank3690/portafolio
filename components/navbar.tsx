ffe806490543c4008ee655a86fcf978c5b782392
juank3690 <juan.gomez5@utp.edu.co>
2024-07-29 07:03:39 -0500

    solucion error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,28 +1,29 @@
 'use client'
+
 import { itemsNavbar } from '@/data'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import MotionTransition from './transition-component'
 
 const Navbar = () => {
-const router=usePathname
+  const router = usePathname()
 
-return (
-  <MotionTransition position="right" className="fixed z-40 flex flex-col items-center justify-center w-full mt-auto h-max bottom-10">
+  return (
+    <MotionTransition position="right" className="fixed z-40 flex flex-col items-center justify-center w-full mt-auto h-max bottom-10">
       <nav>
-          <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-white/15 background-blur-sm">
-              {itemsNavbar.map((item) => (
-                  <div
-                      key={item.id}
-                      className={`px-3 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary ${router === item.link && 'bg-secondary'}`}
-                      data-tooltip-target="tooltip-default">
-                      <Link href={item.link}>{item.icon} </Link>
-                  </div>
-              ))}
-          </div>
+        <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-white/15 background-blur-sm">
+          {itemsNavbar.map((item) => (
+            <div
+              key={item.id}
+              className={`px-3 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary ${router === item.link && 'bg-secondary'}`}
+              data-tooltip-target="tooltip-default">
+              <Link href={item.link}>{item.icon}</Link>
+            </div>
+          ))}
+        </div>
       </nav>
-  </MotionTransition>
-);
+    </MotionTransition>
+  )
 }
 
 export default Navbar
